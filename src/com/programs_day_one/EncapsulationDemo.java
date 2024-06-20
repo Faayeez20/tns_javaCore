626be709ff37db577f64595062987c5edf704ccb
Faayeez20 <faayeez.211447.it@mhssce.ac.in>
2024-06-20 15:36:40 +0530

    First Commit

---
+++ src/com/programs_day_one/EncapsulationDemo.java
@@ -0,0 +1,18 @@
+package com.programs_day_one;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Person ob = new Person();
+		
+		ob.setSerialnum(100);
+		ob.setName("John");
+		ob.setAge(20);
+		
+		System.out.println(ob);
+
+	}
+
+}
---
+++ src/com/programs_day_one/Person.java
@@ -0,0 +1,32 @@
+package com.programs_day_one;
+
+public class Person {
+	int serialnum;
+	String name;
+	int age;
+	
+	public int getSerialnum() {
+		return serialnum;
+	}
+	public void setSerialnum(int serialnum) {
+		this.serialnum = serialnum;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	@Override
+	public String toString() {
+		return "Encapsulation [serialnum=" + serialnum + ", name=" + name + ", age=" + age + "]";
+	}
+
+}
